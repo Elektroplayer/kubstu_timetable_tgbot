248a96644c34960dd890f9b76d03448bcf3a76f5
Electroplayer <vitaly.hawanski@yandex.ru>
2023-05-21 10:47:56 +0300

    Исправление ошибки в ScheduleTimer.ts, из-за которой крашился бот.

--- src/timers/ScheduleTimer.ts
+++ src/timers/ScheduleTimer.ts
@@ -7,18 +7,18 @@ export default class TestTimer extends Timer {
     time = 0;
 
     async init() {
-        new CronJob('0 40 9 * * 0-5', this.exec, null, true, 'Europe/Moscow', {time: 9}); // После первой пары
-        new CronJob('0 20 11 * * 0-5', this.exec, null, true, 'Europe/Moscow', {time: 11}); // второй
-        new CronJob('0 20 13 * * 0-5', this.exec, null, true, 'Europe/Moscow', {time: 12}); // третьей
-        new CronJob('0 00 15 * * 0-5', this.exec, null, true, 'Europe/Moscow', {time: 14}); // четвёртой
-        new CronJob('0 40 16 * * 0-5', this.exec, null, true, 'Europe/Moscow', {time: 16}); // пятой
-        new CronJob('0 20 18 * * 0-5', this.exec, null, true, 'Europe/Moscow', {time: 18}); // шестой
-        new CronJob('0 00 20 * * 0-5', this.exec, null, true, 'Europe/Moscow', {time: 19}); // и даже седьмой
+        new CronJob('0 40 9 * * 0-5', this.exec, null, true, 'Europe/Moscow', { time: 9 }); // После первой пары
+        new CronJob('0 20 11 * * 0-5', this.exec, null, true, 'Europe/Moscow', { time: 11 }); // второй
+        new CronJob('0 20 13 * * 0-5', this.exec, null, true, 'Europe/Moscow', { time: 12 }); // третьей
+        new CronJob('0 00 15 * * 0-5', this.exec, null, true, 'Europe/Moscow', { time: 14 }); // четвёртой
+        new CronJob('0 40 16 * * 0-5', this.exec, null, true, 'Europe/Moscow', { time: 16 }); // пятой
+        new CronJob('0 20 18 * * 0-5', this.exec, null, true, 'Europe/Moscow', { time: 18 }); // шестой
+        new CronJob('0 00 20 * * 0-5', this.exec, null, true, 'Europe/Moscow', { time: 19 }); // и даже седьмой
     }
 
     async exec() {
-        let usersID = await Users.find({notifications: true, group: {$exist: true}}).exec()
-        
+        let usersID = await Users.find({ notifications: true }).exec();
+
         let dateToday = new Date();
         let dateTomorrow = new Date();
         dateTomorrow.setUTCDate(dateTomorrow.getUTCDate() + 1);
@@ -28,29 +28,29 @@ export default class TestTimer extends Timer {
         usersID.forEach(async (elm) => {
             let user = await Cache.getUser(+elm.userId!); // Ищем юзера
 
-            if(!user.group) return; // Если нету группы - забиваем
+            if (!user.group) return; // Если нету группы - забиваем
 
             let text;
-            let todayScheduleArray = await user.group.getRawSchedule(dateToday.getDay(), dateToday.getWeek()%2==0); // Смотрим сегодняшнее расписание
+            let todayScheduleArray = await user.group.getRawSchedule(dateToday.getDay(), dateToday.getWeek() % 2 == 0); // Смотрим сегодняшнее расписание
 
             // Забиваем если
-            if(todayScheduleArray == null) return; // ...расписания нет вообще.
+            if (todayScheduleArray == null) return; // ...расписания нет вообще.
 
-            if(todayScheduleArray.length == 0) {
-                if(this.time != 9) return // ...расписания на сегодня нет, а время уже не 9
-            } else if(this.time != +todayScheduleArray[todayScheduleArray.length-1].time.split(":")[1].split(" - ")[1]) return; // ...ещё не время (отправляем только, после пар)
+            if (todayScheduleArray.length == 0) {
+                if (this.time != 9) return // ...расписания на сегодня нет, а время уже не 9
+            } else if (this.time != +todayScheduleArray[todayScheduleArray.length - 1].time.split(":")[1].split(" - ")[1]) return; // ...ещё не время (отправляем только, после пар)
 
             console.log(` ${user.id}, ${user.group?.name}`)
 
-            text = await user.group.getTextSchedule(dateTomorrow.getDay(), dateTomorrow.getWeek()%2==0);
+            text = await user.group.getTextSchedule(dateTomorrow.getDay(), dateTomorrow.getWeek() % 2 == 0);
 
             let events = await user.group.getTextEvents(dateTomorrow);
-            if(events) text += `\n\n${events}`;
-            
+            if (events) text += `\n\n${events}`;
+
             Cache.bot.sendMessage(
                 user.id,
                 text,
-                {parse_mode: "HTML"}
+                { parse_mode: "HTML" }
             ).catch(err => {
                 console.log(` Bot was blocked by ${user.id}`);
                 console.log(`${err}`);
