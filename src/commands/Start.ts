62a4b581af62eab63f6aa801ada7fa947a319817
Electroplayer <vitaly.hawanski@yandex.ru>
2023-09-10 23:47:54 +0300

    Исправлени мой username

--- src/commands/Start.ts
+++ src/commands/Start.ts
@@ -13,7 +13,7 @@ export default class TodayCommand extends Command {
 
         if(msg.chat.type !== "private") {
             if(!user.group) replytext += "Конкретно у тебя не установлена некоторая важная для меня информация. Давай поговорим в личных сообщениях.";
-            else replytext += "Можешь воспользоваться командами снизу:\n\n/today - Расписание на сегодня\n/tomorrow - Расписание на завтра\n/nearest - Ближайшее расписание\n\nПоддержка: @Elektroplayer_xXx\nGitHub: github.com/Elektroplayer/kubgtu_lessons_tgbot\nПоддержать меня:\nТ: 5536 9141 8751 4363\nС: 2202 2050 2291 3625";
+            else replytext += "Можешь воспользоваться командами снизу:\n\n/today - Расписание на сегодня\n/tomorrow - Расписание на завтра\n/nearest - Ближайшее расписание\n\nПоддержка: @Elektroplayer\nGitHub: github.com/Elektroplayer/kubgtu_lessons_tgbot\nПоддержать меня:\nТ: 5536 9141 8751 4363\nС: 2202 2050 2291 3625";
             
             Cache.bot.sendMessage(msg.chat.id, replytext, {
                 disable_web_page_preview: true
@@ -22,7 +22,7 @@ export default class TodayCommand extends Command {
             if(!user.group) {
                 user.scene = Cache.scenes.find(s => s.name == "settings")
 
-                replytext += "У тебя не установлена некоторая важная для меня информация. Подскажи пожалуйста,\n\nКакой у тебя институт. Если твоего тут нет, значит он может появиться в будущем";
+                replytext += "У тебя не установлена некоторая важная для меня информация. Подскажи пожалуйста,\n\nКакой у тебя институт. Если твоего тут нет, значит можешь написать мне в ЛС (@Elektroplayer), чтобы я его добавил. Если что-то пошло не так, пиши туда же - я не кусаюсь.";
 
                 Cache.bot.sendMessage(msg.chat.id, replytext, {
                     disable_web_page_preview: true,
@@ -33,7 +33,7 @@ export default class TodayCommand extends Command {
                 });
 
             } else {
-                replytext += "Поддержка: @Elektroplayer_xXx\nGitHub: github.com/Elektroplayer/kubgtu_lessons_tgbot\nПоддержать меня:\nТ: 5536 9141 8751 4363\nС: 2202 2050 2291 3625\n\nМожешь выбрать действие снизу.";
+                replytext += "Поддержка: @Elektroplayer\nGitHub: github.com/Elektroplayer/kubgtu_lessons_tgbot\nПоддержать меня:\nТ: 5536 9141 8751 4363\nС: 2202 2050 2291 3625\n\nМожешь выбрать действие снизу.";
 
                 user.scene = Cache.scenes.find(s => s.name == "main");
 
