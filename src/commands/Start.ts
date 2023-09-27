01f197f4bf56c1e5a5357769b22daeaaff492ace
Electroplayer <vitaly.hawanski@yandex.ru>
2023-09-27 22:48:20 +0300

    Доисправление start
    Точно последний комит

--- src/commands/Start.ts
+++ src/commands/Start.ts
@@ -9,13 +9,14 @@ export default class TodayCommand extends Command {
     sceneName = [];
 
     async exec(user: User, msg: Message): Promise<void> {
-        let replytext = `<b>Приветствую, ${msg.from!.username}<b>\n\n`;
+        let replytext = `<b>Приветствую, ${msg.from!.username}</b>\n\n`;
 
         if(msg.chat.type !== "private") {
             if(!user.group) replytext += "Конкретно у тебя не установлена некоторая важная для меня информация. Давай поговорим в личных сообщениях.";
             else replytext += "Можешь воспользоваться командами снизу:\n\n/today - Расписание на сегодня\n/tomorrow - Расписание на завтра\n/nearest - Ближайшее расписание\n/about - Информация о боте";
             
             Cache.bot.sendMessage(msg.chat.id, replytext, {
+                parse_mode: "HTML",
                 disable_web_page_preview: true
             });
         } else {
@@ -26,6 +27,7 @@ export default class TodayCommand extends Command {
 
                 Cache.bot.sendMessage(msg.chat.id, replytext, {
                     disable_web_page_preview: true,
+                    parse_mode: "HTML",
                     reply_markup: {
                         inline_keyboard: instKeyboard,
                         resize_keyboard: true,
