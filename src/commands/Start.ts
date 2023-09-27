3dedf34f86301177d4b081e9d183fbdff6857e62
Electroplayer <vitaly.hawanski@yandex.ru>
2023-09-27 22:32:47 +0300

    Изменена команда start

--- src/commands/Start.ts
+++ src/commands/Start.ts
@@ -13,7 +13,7 @@ export default class TodayCommand extends Command {
 
         if(msg.chat.type !== "private") {
             if(!user.group) replytext += "Конкретно у тебя не установлена некоторая важная для меня информация. Давай поговорим в личных сообщениях.";
-            else replytext += "Можешь воспользоваться командами снизу:\n\n/today - Расписание на сегодня\n/tomorrow - Расписание на завтра\n/nearest - Ближайшее расписание\n\nПоддержка: @Elektroplayer\nGitHub: github.com/Elektroplayer/kubgtu_lessons_tgbot\nПоддержать меня:\nТ: 5536 9141 8751 4363\nС: 2202 2050 2291 3625";
+            else replytext += "Можешь воспользоваться командами снизу:\n\n/today - Расписание на сегодня\n/tomorrow - Расписание на завтра\n/nearest - Ближайшее расписание\n/about - Информация о боте";
             
             Cache.bot.sendMessage(msg.chat.id, replytext, {
                 disable_web_page_preview: true
@@ -33,7 +33,13 @@ export default class TodayCommand extends Command {
                 });
 
             } else {
-                replytext += "Поддержка: @Elektroplayer\nGitHub: github.com/Elektroplayer/kubgtu_lessons_tgbot\nПоддержать меня:\nТ: 5536 9141 8751 4363\nС: 2202 2050 2291 3625\n\nМожешь выбрать действие снизу.";
+                replytext += `Поддержка: @Elektroplayer\n` +
+                `Новости: @kubstu_schedule_news\n` +
+                `GitHub: <a href="https://github.com/Elektroplayer/kubstu_timetable_tgbot">Elektroplayer/kubstu_timetable_tgbot</a>\n` +
+                `Обладаемая информация: <a href="https://github.com/Elektroplayer/kubstu_timetable_tgbot#%D1%87%D1%82%D0%BE-%D1%8F-%D0%BE-%D0%B2%D0%B0%D1%81-%D0%B7%D0%BD%D0%B0%D1%8E">тут</a>\n` +
+                `Поддержать меня:\n` +
+                `Т: 5536 9141 8751 4363\n` +
+                `С: 2202 2050 2291 3625`,
 
                 user.scene = Cache.scenes.find(s => s.name == "main");
 
